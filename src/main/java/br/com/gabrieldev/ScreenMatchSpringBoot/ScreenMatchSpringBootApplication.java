c440b0c84a4cd689c6fcf11e6386fc6d9227505e
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-07-04 16:46:19 -0300

    Setup Base do projeto em Spring

---
+++ src/main/java/br/com/gabrieldev/ScreenMatchSpringBoot/ScreenMatchSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldev.ScreenMatchSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrieldev/ScreenMatchSpringBoot/ScreenMatchSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldev.ScreenMatchSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMatchSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
